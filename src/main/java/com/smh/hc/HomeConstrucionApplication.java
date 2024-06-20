92b2d3423df338333e84f8d74f195c9d28692432
Sunil <sunilhanumante1@gmail.com>
2024-06-20 16:32:11 +0530

    This is HomeComstruction Project

---
+++ src/main/java/com/smh/hc/HomeConstrucionApplication.java
@@ -0,0 +1,13 @@
+package com.smh.hc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeConstrucionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeConstrucionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smh/hc/HomeConstrucionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smh.hc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeConstrucionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
